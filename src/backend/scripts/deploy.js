7ca225b7a7f79040654e57317b862e001e336ea0
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-12-20 09:52:26 +0300

    contructor

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
@@ -6,10 +8,15 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // deploy contracts here:
+  const NFTMarketPlaceFactory = await ethers.getContractFactory("MusicNFTMarketplace");
+  const nftMarketplace = await NFTMarketPlaceFactory.deploy();
+
+  console.log("Smart contract address", nftMarketplace.address);
+
   
   
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nftMarketplace, "MusicNFTMarketplace");
 }
 
 function saveFrontendFiles(contract, name) {
